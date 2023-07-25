86b2c368b811f1ebb5930a2731119f7ddc962fad
muhammed naseeb <nasinaseeb13@gmail.com>
2023-07-25 12:24:32 +0530

    card changed

--- src/components/user/Wallet/Wallet.jsx
+++ src/components/user/Wallet/Wallet.jsx
@@ -1,10 +1,4 @@
-import {
-    Card,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
+
 import { useEffect, useState } from "react";
 import { walletFinding } from "../../../axios/services/user/User";
 import { useSelector } from "react-redux";
@@ -23,8 +17,8 @@ import { useSelector } from "react-redux";
     return (
     <div className=" h-screen flex justify-center items-center bg-slate-200">
 
-      <Card className="mt-6 w-96 drop-shadow-[6px_8px_6px_rgba(1,1,0,0.5)]">
-        <CardBody>
+      <div className="mt-6 w-96 drop-shadow-[6px_8px_6px_rgba(1,1,0,0.5)]">
+        <div className="border-solid border-2 border-teal-900 ">
          <div className="flex justify-center">
             <div className="border-solid border-b-2 border-teal-900 p-8 inline-block">
                CURRENT BALANCE
@@ -32,14 +26,14 @@ import { useSelector } from "react-redux";
             </div>
             <div className="flex justify-center">
 
-                <div className="pt-7 text-8xl">
+                <div className="pt-7 text-8xl pb-2">
                  {balance?.currentBalance}
                 </div>
             </div>
          
          
-        </CardBody>
-      </Card>
+        </div>
+      </div>
     </div>
     );
   }
