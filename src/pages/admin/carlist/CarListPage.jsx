f263d383dc36837787ee7192bf9c212e2909e5ef
Muhammed_Nazeeb <125156699+blACKSOULMHD@users.noreply.github.com>
2023-07-25 11:54:42 +0530

    Update CarListPage.jsx

--- src/pages/admin/carlist/CarListPage.jsx
+++ src/pages/admin/carlist/CarListPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../../components/admin/SideBarComponents/components/Layout'
-import CarList from '../../../components/admin/carlist/carList'
+import CarList from '../../../components/admin/carlist/CarList'
 
 function CarListPage() {
   return (
