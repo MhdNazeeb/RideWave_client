b049ece3337a6f7c5effd5a391adab0b26e23b1a
Muhammed_Nazeeb <125156699+blACKSOULMHD@users.noreply.github.com>
2023-07-25 09:22:02 +0530

    Update SuccessPage.jsx

--- src/pages/user/successpage/SuccessPage.jsx
+++ src/pages/user/successpage/SuccessPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Success from '../../../components/user/success/success'
+import Success from '../../../components/user/success/Success'
 import Header from '../../../components/user/Header/Header'
 
 export default function SuccessPage() {
