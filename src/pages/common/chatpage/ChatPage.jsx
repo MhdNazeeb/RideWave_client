61ac17ba26fa50fd999273ba88e419de1310f4ed
Muhammed_Nazeeb <125156699+blACKSOULMHD@users.noreply.github.com>
2023-07-25 09:29:09 +0530

    Update ChatPage.jsx

--- src/pages/common/chatpage/ChatPage.jsx
+++ src/pages/common/chatpage/ChatPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Chat from '../../../components/common/chat/chat'
+import Chat from '../../../components/common/chat/Chat'
 import Header from '../../../components/user/Header/Header'
 
 function ChatPage() {
