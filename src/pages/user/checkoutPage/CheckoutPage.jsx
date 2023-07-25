a0945dcaf202549eeffe609d4f1d080837016c6e
Muhammed_Nazeeb <125156699+blACKSOULMHD@users.noreply.github.com>
2023-07-25 09:26:05 +0530

    Update CheckoutPage.jsx

--- src/pages/user/checkoutPage/CheckoutPage.jsx
+++ src/pages/user/checkoutPage/CheckoutPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../../components/user/Header/Header'
-import Footer from '../../../components/user/footer/footer'
+import Footer from '../../../components/user/footer/Footer'
 import Checkout from '../../../components/user/checkout/Checkout'
 
 function CheckoutPage() {
